62d5b619cbeceee9966a28f8fa0a7deb11365c17
Christophe Eymard <ceymard@pm.me>
2025-11-28 08:43:36 +0100

    fix name tracking

--- index.ts
+++ index.ts
@@ -4,14 +4,11 @@ import { swayidleEventStream } from "./swayidle"
 import merge from "fast-merge-async-iterators"
 
 let windows = new Map<number, NiriWindow>()
-let workspaces = new Map<number, Workspace>()
 let windows_in_workspace = new Map<number, Map<number, NiriWindow>>()
 let focused_workspace_id: number = -1
 let focused_window_id: number = -1
 
-export const Replacements = [
-  /^.+? - (.+?) - (?=cursor|code).*$/i,
-]
+export const Replacements = [/^.+? - (.+?) - (?=cursor|code).*$/i]
 
 function test_replacements(window: NiriWindow) {
   for (let replacement of Replacements) {
@@ -23,31 +20,40 @@ function test_replacements(window: NiriWindow) {
   return null
 }
 
-let last_name = null as string | null
-export function evan_name_change_from_window(window_id: number) {
+let current_name = null as string | null
+export function name_change_from_window(
+  window_id: number | null,
+  workspace_id?: number
+) {
   let changed = false
-  const window = windows.get(window_id)
-  if (!window) {
-    return null
+  const window = windows.get(window_id!)
+  if (window) {
+    const replacements = test_replacements(window)
+    if (replacements) {
+      changed = current_name !== replacements
+      current_name = replacements
+      return changed
+    }
+    workspace_id = window.workspace_id
   }
-  const replacements = test_replacements(window)
-  if (replacements) {
-    changed = last_name !== replacements
-    last_name = replacements
-  }
-  if (replacements == null) {
-    for (let wd of windows_in_workspace.get(window.workspace_id)?.values() ?? []) {
+
+  if (workspace_id) {
+    for (let wd of windows_in_workspace.get(workspace_id)?.values() ?? []) {
       if (wd.id === window_id) {
         continue
       }
       const replacements = test_replacements(wd)
       if (replacements) {
-        changed = last_name !== replacements
-        last_name = replacements
+        changed = current_name !== replacements
+        current_name = replacements
+        return changed
       }
     }
   }
-  return changed ? last_name : null
+
+  changed = current_name !== null
+  current_name = null
+  return changed
 }
 
 function update_window(win: NiriWindow) {
@@ -57,7 +63,7 @@ function update_window(win: NiriWindow) {
       windows_in_workspace.get(prev.workspace_id)?.delete(prev.id)
     }
   }
-  
+
   let wwk = windows_in_workspace.get(win.workspace_id)
   if (wwk === undefined) {
     wwk = new Map()
@@ -75,13 +81,12 @@ function remove_window(id: number) {
   windows.delete(id)
 }
 
-async function run() {
+async function* changes_stream() {
   const mg = merge(swayidleEventStream(2), niriEventStream())
 
   for await (const event of mg) {
-
     switch (event.type) {
-      case "WindowsChanged":
+      case "WindowsChanged": {
         windows_in_workspace = new Map()
         windows = new Map()
         for (let win of event.windows) {
@@ -90,45 +95,45 @@ async function run() {
           }
           update_window(win)
         }
-        const name = evan_name_change_from_window(focused_window_id)
-        console.log("name:", name)
-        break
-      case "WorkspacesChanged":
-        workspaces = new Map(
-          event.workspaces.map((workspace) => {
-            if (workspace.is_focused) {
-              focused_workspace_id = workspace.id
-            }
-            return [workspace.id, workspace]
-          })
-        )
-        break
-      case "WorkspaceActivated":
-        focused_workspace_id = event.id
-        break
-      case "WindowFocusChanged": {
-        focused_window_id = event.id
-        const window = windows.get(focused_window_id)
-        const n2 = evan_name_change_from_window(focused_window_id)
-        console.log("name:", n2)
-        break 
-      }
-      case "WindowOpenedOrChanged":
-        update_window(event.window)
-        const n2 = evan_name_change_from_window(event.window.id)
-        console.log("name:", n2)
-        break
-      case "WindowClosed":
-        remove_window(event.id)
-        break
-      case "WorkspaceActiveWindowChanged":
-        const wrks = workspaces.get(event.workspace_id)
-        if (wrks) {
-          wrks.active_window_id = event.active_window_id
+        if (name_change_from_window(focused_window_id)) {
+          yield { type: "NameChange", name: current_name }
         }
         break
+      }
+      case "WorkspacesChanged": {
+        break
+      }
+      case "WorkspaceActivated": {
+        focused_workspace_id = event.id
+        if (name_change_from_window(null, focused_workspace_id)) {
+          yield { type: "NameChange", name: current_name }
+        }
+        break
+      }
+      case "WorkspaceActiveWindowChanged": {
+        // const wrks = workspaces.get(event.workspace_id)
+        // console.log("??")
+        // if (wrks) {
+        //   wrks.active_window_id = event.active_window_id
+        // }
+        break
+      }
+      case "WindowOpenedOrChanged": {
+        update_window(event.window)
+        if (name_change_from_window(event.window.id)) {
+          yield { type: "NameChange", name: current_name }
+        }
+        break
+      }
+      case "WindowFocusChanged": {
+        focused_window_id = event.id
+        if (name_change_from_window(focused_window_id)) {
+          yield { type: "NameChange", name: current_name }
+        }
+        break
+      }
       case "ConfigLoaded":
-        console.log("config loaded:", event.failed)
+        // console.log("config loaded:", event.failed)
         break
       case "WindowLayoutChanged":
         break
@@ -138,16 +143,25 @@ async function run() {
         break
       case "OverviewOpenedOrClosed":
         break
+      case "WindowClosed":
+        remove_window(event.id)
+        break
       case "Timeout":
         // When we get to a timeout, we yield a name with a duration
 
         break
       case "Resume":
-        
         break
-      default:
+      default: {
         console.log("event:", event)
+      }
     }
+  }
+}
+
+async function run() {
+  for await (const event of changes_stream()) {
+    console.log("event:", event)
   }
 }
 
